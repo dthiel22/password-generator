d820f903c20be355b6d40e43c3135eb9404efcab
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2022-09-29 14:54:41 -0700

    stored recorded prompt values in array, made character pool selector based on array answers

--- Develop/script.js
+++ Develop/script.js
@@ -17,7 +17,7 @@ var specialCharacters = "!@#$%^&*_-+="
 
 //TODO: array of numbers, specials, uppercase, lowercase
 var passwordArray = [passwordLength,passwordLowercase,passwordUppercase,passwordNumeric,passwordSpecial]
-var usedMaterial = [lowercaseCharacters, uppercaseCharacters, numericCharacters, specialCharacters]
+var usedChar = ""
 
 
 // Write password to the #password input
@@ -32,18 +32,24 @@ function writePassword() {
 
 function generatePassword(){
 //TODO: prompt user for length
-    passwordLength=prompt("How many characters would you like your password to be?\n\nPassword can be 8-128 words.")
-    parseInt(passwordLength)
-    console.log("password length")
-    console.log(passwordLength)
+  result = "";
+  passwordLength = "";
+  usedChar = "";
+  passwordLength=prompt("How many characters would you like your password to be?\n\nPassword can be 8-128 words.")
+  parseInt(passwordLength)
+  console.log("password length")
+  console.log(passwordLength)
 
 //TODO: check length
     if (passwordLength < 8 || passwordLength > 128) {
-      alert("This number is invalid, please read the directions again")
-      generatePassword()
+      alert("This number is invalid, please click on 'Generate Password' and read the directions again")
+      passwordLength = "";
     }else{
-
+    // function passwordPool (){
+      // passwordArray[1,2,3,4]=false;
 //TODO: promtpt/confirm for types of characters
+
+// TODO: maybe add a function?
     passwordLowercase=confirm("Would you like your password to contain lowercase letters?\n\nClick 'OK' for yes and 'Cancel' for no.")
     console.log("password lowercase") 
     console.log(passwordLowercase)
@@ -60,62 +66,52 @@ function generatePassword(){
     console.log("password special") 
     console.log(passwordSpecial)
     }
-
+    
     //TODO: store responses
     var passwordArray = [passwordLength,passwordLowercase,passwordUppercase,passwordNumeric,passwordSpecial]
     console.log(passwordArray)
-    return passwordArray
-    
-}
-//TODO: based on the responses trim/build available pool of characters
-// var passwordArray = [passwordLength,passwordLowercase,passwordUppercase,passwordNumeric,passwordSpecial]
-// var usedMaterial = [lowercaseCharacters, uppercaseCharacters, numericCharacters, specialCharacters]
 
-    if (passwordArray[1] == true){
-      console.log("it is true!")
+//TODO: based on the responses trim/build available pool of characters
+
+
+    if (passwordArray[1] === true){
+      usedChar += "abcdefghijklmnopqrstuvwxyz"
+      console.log("add lowercase"); 
+    }if (passwordArray[2] === true){
+      usedChar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      console.log("add uppercase");   
+    }if (passwordArray[3] === true){
+      usedChar += "1234567890"
+      console.log("add numbers");
+    }if (passwordArray[4] === true){
+      usedChar += "!@#$%^&*_-+="
+      console.log("add symbols");
+    }if (!passwordArray[1] && !passwordArray[2] && !passwordArray[3] && !passwordArray[4]){
+      alert("Please select at least one option.")
+      return ""
+      // passwordPool ()
+      console.log(passwordArray[1,2,3,4])
+    }else {
+      console.log(usedChar)
     }
-  
 //TODO: build the password character by character >>> select a random character from array of multiple characters
 
 //TODO: generate random int within range of available char length
-
-    // Math.floor(Math.random() * passwordLength.length)
 
 //TODO: addd char to result
 
 //TODO: do until length = desired length
 
-// return result
+  while (result.length < passwordLength) {
+    result += usedChar[Math.floor(Math.random() * usedChar.length)];
+    } 
+    console.log(result);
+    console.log(result.length); 
 
-
+  return result
+}
 // }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-//============================MY CODE==========================
-
-// function writePassword() {
-//   let passwordLength = prompt("How many characters would you like your password to contain?");
-//   if (passwordLength != null) {
-//     console.log(passwordLength)
-//   }
-  
-//   let passwordLowercase = "Would you like your password to contain lowercase letters?\n\nClick 'OK' for yes and 'Cancel' for no.";
-//   if (confirm(passwordLowercase) == true) {
-//     passwordLowercase = true;
-//   } else {
-//     passwordLowercase = false;
-//   }
-//   console.log(passwordLowercase)
-
-//   let passwordUppercase = "Would you like your password to contain lowercase letters?\n\nClick 'OK' for yes and 'Cancel' for no.";
-//   if (confirm(passwordUppercase) == true) {
-//     passwordUppercase = true;
-//   } else {
-//     passwordUppercase = false;
-//   }
-//   console.log(passwordUppercase)
-// }
